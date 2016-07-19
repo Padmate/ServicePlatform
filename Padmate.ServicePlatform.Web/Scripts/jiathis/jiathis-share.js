99789563d3f4c46bb8136e814ab8fd42230a6843
Padmate <959853840@qq.com>
2016-07-19 18:07:05 +0800

    一个页面多个“一键分享”按钮

---
+++ Padmate.ServicePlatform.Web/Scripts/jiathis/jiathis-share.js
@@ -0,0 +1,42 @@
+﻿function setShare(articleId) {
+
+    $.ajax({
+        type: "POST",
+        url: "/Article/GetArticleById",
+        data: { articleid: articleId },
+        dataType: "json",
+        async: false,   //同步
+        success: function (result) {
+
+            jiathis_config.title = result.SubTitle;
+        }
+    });
+
+}
+
+function setShare(articleId, url) {
+
+    $.ajax({
+        type: "POST",
+        url: "/Article/GetArticleById",
+        data: { articleid: articleId },
+        dataType: "json",
+        async: false,   //同步
+        success: function (result) {
+            debugger;
+            jiathis_config.title = result.SubTitle;
+            jiathis_config.url = url;
+        }
+    });
+
+   
+}
+
+
+var jiathis_config = {
+    data_track_clickback: true,
+    summary: "",
+    pic: "http://xiamenip.cn/img/logos/favicon.jpg",
+    shortUrl: false,
+    hideMore: false
+}
